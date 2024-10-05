2e178bf6ed27250aa6bc7cbeec5e141ad10a1553
ogi-iii <kazuki.aape.09@gmail.com>
2024-10-06 00:09:02 +0900

    [test] yaml config loader

--- src/loaders/config.loader.spec.ts
+++ src/loaders/config.loader.spec.ts
@@ -4,6 +4,17 @@ describe("load config from yaml files", () => {
     it("in the default target dir", () => {
         const yamlConfig = loadYamlConfig();
         expect(yamlConfig.endpoints.length).toEqual(4);
+        yamlConfig.endpoints.forEach((endpoint) => {
+            expect(endpoint).toHaveProperty('path');
+            expect(endpoint).toHaveProperty('method');
+            expect(endpoint).toHaveProperty('requestConfig');
+            expect(endpoint.requestConfig).toHaveProperty('url');
+            expect(endpoint.requestConfig).toHaveProperty('method');
+            if (endpoint.requestConfig.method.toUpperCase() === 'POST') {
+                expect(endpoint.requestConfig).toHaveProperty('headers');
+                expect(endpoint.requestConfig).toHaveProperty('body');
+            }
+        });
     });
 
     it("in the target dir which does NOT exist", () => {
