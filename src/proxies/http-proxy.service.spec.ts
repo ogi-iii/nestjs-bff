899aef2054354dbbca3ebc5236339f884a6e04e2
ogi-iii <kazuki.aape.09@gmail.com>
2024-10-06 10:42:48 +0900

    [test] http request proxy headers

--- src/proxies/http-proxy.service.spec.ts
+++ src/proxies/http-proxy.service.spec.ts
@@ -184,4 +184,24 @@ describe('proxy http request', () => {
     );
     expect(response.status).toEqual(404);
   });
+
+  it('as GET method to the url which returns request headers', async () => {
+    const requestConfigDto: HttpRequestConfigDto = {
+      url: 'https://httpbin.org/headers',
+      method: 'GET',
+      headers: {
+        Accept: '{{accept}}',
+      },
+      body: {},
+    };
+    const requestData = {
+      accept: 'application/json',
+    };
+    const response = await httpProxyService.proxyHttpRequest(
+      requestConfigDto,
+      requestData,
+    );
+    expect(response.status).toEqual(200);
+    expect(response.data.headers).toHaveProperty('Accept', requestData.accept);
+  });
 });
