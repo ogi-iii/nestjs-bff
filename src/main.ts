86f087710ab87eaefb11e232f2a61bfca83056b3
ogi-iii <kazuki.aape.09@gmail.com>
2024-10-07 05:26:49 +0900

    [fix] app port

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
 
 /**
  * Run NestJS app.
  */
 async function bootstrap() {
+  const DEFAULT_PORT = 3001;
   try {
+    config();
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const port = process.env.PORT ? process.env.PORT : DEFAULT_PORT;
+    await app.listen(port);
   } catch (err) {
     console.error('failed to run app:', err);
     process.exit(1);
