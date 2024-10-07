e3c077fb9516cea1b7a1bc992cb11b24ccfd07f7
ogi-iii <kazuki.aape.09@gmail.com>
2024-10-07 23:49:17 +0900

    [feat] shellscript to run a keycloak container

---
+++ run-keycloak-container.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+local_port=${1}
+admin_username=${2}
+admin_password=${3}
+docker run -e KEYCLOAK_ADMIN=${admin_username:-"admin"} -e KEYCLOAK_ADMIN_PASSWORD=${admin_password:-"password"} -p ${local_port:-"8080"}:8080 quay.io/keycloak/keycloak:25.0 start-dev
