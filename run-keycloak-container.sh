6229c68ffb68cd795f37679d497f411715a494a9
ogi-iii <kazuki.aape.09@gmail.com>
2024-10-09 01:30:49 +0900

    [fix] port forwarding

--- run-keycloak-container.sh
+++ run-keycloak-container.sh
@@ -2,4 +2,4 @@
 local_port=${1}
 admin_username=${2}
 admin_password=${3}
-docker run -e KEYCLOAK_ADMIN=${admin_username:-"admin"} -e KEYCLOAK_ADMIN_PASSWORD=${admin_password:-"password"} -p ${local_port:-"8080"}:8080 quay.io/keycloak/keycloak:25.0 start-dev
+docker run -e KEYCLOAK_ADMIN=${admin_username:-"admin"} -e KEYCLOAK_ADMIN_PASSWORD=${admin_password:-"password"} -p ${local_port:-"127.0.0.1:8083"}:8080 quay.io/keycloak/keycloak:25.0 start-dev
