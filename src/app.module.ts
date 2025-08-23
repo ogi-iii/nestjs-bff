389d4d587a4ebd13046f0362a900386c87c36d60
ogi-iii <kazuki.aape.09@gmail.com>
2025-08-24 03:13:00 +0900

    add CacheModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { CacheModule } from '@nestjs/cache-manager';
 import { ConfigModule } from '@nestjs/config';
 import { HttpProxyModule } from './modules/http-proxy.module';
+import * as redisStore from 'cache-manager-redis-store';
 
 /**
  * NestJS Root App Module
  */
 @Module({
-  imports: [ConfigModule.forRoot(), HttpProxyModule.register()],
+  imports: [
+    ConfigModule.forRoot(),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+    }),
+    HttpProxyModule.register(),
+  ],
 })
 export class AppModule {}
